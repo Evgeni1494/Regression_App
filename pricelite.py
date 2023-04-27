28e27f6bcbb2f7abeeb3dbba0c0785d81675b857
Evgeni1494 <yoni1@hotmail.fr>
2023-04-27 11:05:21 +0200

    MVP

--- pricelite.py
+++ pricelite.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import joblib
-from Regress import preprocessor
-
+import pandas as pd
 # Charger le modèle
 model = joblib.load('Price_Reg_Model.joblib')
 
@@ -12,68 +11,21 @@ model = joblib.load('Price_Reg_Model.joblib')
 st.title('Application de prédiction de prix des voitures')
 
 
-new_car ={
-    "car_ID": [1],
-    
-    "etat_de_route": [3],
-    
-    "carburant": ["gas"],
-    
-    "turbo": ["standard"],
-    
-    "nombre_portes": [2],
-    
-    "type_vehicule": ["decapotable"],
-    
-    "roues_motrices": ["arriere"],
-    
-    "emplacement_moteur": ["devant"],
-    
-    "empattement": [2.25],
-    
-    "longueur_voiture": [4.29],
-    
-    "largeur_voiture": [1.63],
-    
-    "hauteur_voiture": [1.24],
-    
-    "poids_vehicule": [1.27],
-    
-    "type_moteur": ["ACT"],
-    
-    
-    "nombre_cylindres": [4],
-    
-    "taille_moteur": [34904],
-    "systeme_carburant": ["carburateur 2 corps"],
-    "taux_alésage": [3.47],
-    "course": [2.68],
-    "taux_compression": [9],
-    "chevaux": [111],
-    "tour_moteur": [5000],
-    "consommation_ville": [21],
-    "consommation_autoroute": [27],
-    "marque": ["audi"],
-    "modele": ["fox"],
-    
-}
-
-
 # Créer les champs pour les entrées utilisateur
 car_ID = 1
 
-etat_de_route = st.selectbox('Etat de route', [-3,-2,-1,0,1,2,3])
+etat_de_route = st.selectbox('Etat de route', [3,2,1,0,-1,-2,-3])
 carburant = st.selectbox('Carburant',['gas','diesel'])
 turbo = st.selectbox('Turbo',['turbo','standard'])
 nombre_portes = st.selectbox('Nombre Portes',[2,4])
 type_vehicule = st.selectbox('Type Vehicule',['decapotable', 'hayon', 'berline', 'break', 'coupe'])
 roues_motrices = st.selectbox('Roues Motrices',['arriere', 'avant', '4motrice'])
 emplacement_moteur = st.selectbox('Emplacement Moteur',['devant', 'derrier'])
-empattement = st.number_input('Empattement en metres', min_value=0, max_value=40)
-longueur_voiture = st.number_input('Longuer en metres', min_value=0, max_value=40)
-largeur_voiture = st.number_input('Largeur en metres', min_value=0, max_value=40)
-hauteur_voiture = st.number_input('Hauteur en metres', min_value=0, max_value=40)
-poids_vehicule = st.number_input('Poids en tonnes', min_value=0, max_value=50)
+empattement = st.number_input('Empattement en metres', min_value=0.0, max_value=400.0,step=0.01)
+longueur_voiture = st.number_input('Longuer en metres', min_value=0.0, max_value=400.0,step=0.01)
+largeur_voiture = st.number_input('Largeur en metres', min_value=0.0, max_value=400.0,step=0.01)
+hauteur_voiture = st.number_input('Hauteur en metres', min_value=0.0, max_value=400.0,step=0.01)
+poids_vehicule = st.number_input('Poids en tonnes', min_value=0.0, max_value=500.0,step=0.01)
 type_moteur = st.selectbox('Type de Moteur',[
         'double ACT', 
         'soupapes en tête à manchon', 
@@ -81,25 +33,119 @@ type_moteur = st.selectbox('Type de Moteur',[
         'ligne des cylindres', 'moteur rotatif',
         'soupapes en tête à flux croisés', 'double ACT à soupapes en V'] )
 nombre_cylindres = st.selectbox('Nombre de cylindres',[ 2,3,4,5,6,8,12])
-taille_moteur = st.number_input('Taille moteur', min_value=0, max_value=500000)
-
-year = st.number_input('Année de la voiture', min_value=1950, max_value=2023, step=1)
-mileage = st.number_input('Kilométrage de la voiture', min_value=0, max_value=1000000, step=1000)
-fuel_type = st.selectbox('Type de carburant', ['Essence', 'Diesel', 'GPL', 'Hybride', 'Electrique'])
-transmission = st.selectbox('Type de transmission', ['Manuelle', 'Automatique'])
-marque = st.selectbox('Marque de la voiture', ['Audi', 'BMW', 'Chevrolet', 'Dodge', 'Ford', 'Honda', 'Hyundai', 'Jeep', 'Kia', 'Mercedes-Benz', 'Mitsubishi', 'Nissan', 'Porsche', 'Subaru', 'Toyota', 'Volkswagen'])
+taille_moteur = st.number_input('Taille moteur', min_value=0.0, max_value=500000.0,step=0.01)
+systeme_carburant = st.selectbox('Systeme carburant', 
+                                 ['injection multipoint multipoint port unique',
+                                'carburateur 2 corps', 'injection carburant multipoint',
+                                'carburateur 1 corps', 'injection monopoint',
+                                'carburateur 4 corps', 'injection indirecte',
+                                'injection D monopoint'])
+taux_alésage = st.number_input('Taux alésage', min_value=0.0, max_value=20.0,step=0.01)
+course = st.number_input('Course', min_value=0.0, max_value=20.0,step=0.01)
+taux_compression = st.number_input('Taux compression', min_value=0.0, max_value=40.0,step=0.01)
+chevaux = st.number_input('Puissance en chevaux',min_value=0.0, max_value=2000.0,step=0.01)
+tour_moteur = st.number_input('Tours moteur',min_value=0.0, max_value=10000.0,step=0.01)
+consommation_ville = st.number_input('Consomation en ville',min_value=0.0, max_value=10000.0,step=0.01)
+consommation_autoroute = st.number_input('Consomation en autoroute',min_value=0.0, max_value=10000.0,step=0.01)
+marque = st.selectbox('Marque de la voiture', 
+                      ['alfa-romeo', 'audi', 
+                    'bmw', 'chevrolet', 'dodge', 'honda',
+                    'isuzu', 'jaguar', 'mazda', 'buick', 'mercury', 'mitsubishi',
+                    'nissan', 'peugeot', 'plymouth', 'porsche', 'renault', 'saab',
+                    'subaru', 'toyota', 'volkswagen', 'volvo'])
 modele = st.text_input('Modèle de la voiture')
 
 # Préparer les entrées utilisateur pour la prédiction
-user_inputs = [[car_ID, car_model, year, mileage, fuel_type, transmission]]
+# user_inputs = [[car_ID, 
+#                 etat_de_route, 
+#                 carburant, turbo, 
+#                 nombre_portes, 
+#                 type_vehicule,
+#                 roues_motrices,
+#                 emplacement_moteur,
+#                 empattement,
+#                 longueur_voiture,
+#                 largeur_voiture,
+#                 hauteur_voiture,
+#                 poids_vehicule,
+#                 type_moteur,
+#                 nombre_cylindres,
+#                 taille_moteur,
+#                 systeme_carburant,
+#                 taux_alésage,
+#                 course,
+#                 taux_compression,
+#                 chevaux,
+#                 tour_moteur,
+#                 consommation_ville,
+#                 consommation_autoroute,
+#                 marque,
+#                 modele,
+#                 ]]
 
-# Appliquer la transformation de prétraitement
-prediction_inputs = preprocessor.transform(user_inputs)
+user_inputs = pd.DataFrame(data ={
+    "car_ID":car_ID, 
+                "etat_de_route" : etat_de_route, 
+                "carburant" : carburant, 
+                "turbo" : turbo, 
+                "nombre_portes" : nombre_portes, 
+                "type_vehicule" : type_vehicule,
+                "roues_motrices" : roues_motrices,
+                "emplacement_moteur" : emplacement_moteur,
+                "empattement" : empattement,
+                "longueur_voiture" : longueur_voiture,
+                "largeur_voiture" : largeur_voiture,
+                "hauteur_voiture" : hauteur_voiture,
+                "poids_vehicule" : poids_vehicule,
+                "type_moteur" : type_moteur,
+                "nombre_cylindres" : nombre_cylindres,
+                "taille_moteur" : taille_moteur,
+                "systeme_carburant" : systeme_carburant,
+                "taux_alésage" : taux_alésage,
+                "course" : course,
+                "taux_compression" : taux_compression,
+                "chevaux" : chevaux,
+                "tour_moteur" : tour_moteur,
+                "consommation_ville" : consommation_ville,
+                "consommation_autoroute" : consommation_autoroute,
+                "marque" : marque,
+                "modele" : modele
+                }, index=[0])
 
-# Effectuer la prédiction avec le modèle
-prediction = model.predict(prediction_inputs)
+if st.button("Predire"):
+    user_inputs = pd.DataFrame(data ={
+    "car_ID":car_ID, 
+                "etat_de_route" : etat_de_route, 
+                "carburant" : carburant, 
+                "turbo" : turbo, 
+                "nombre_portes" : nombre_portes, 
+                "type_vehicule" : type_vehicule,
+                "roues_motrices" : roues_motrices,
+                "emplacement_moteur" : emplacement_moteur,
+                "empattement" : empattement,
+                "longueur_voiture" : longueur_voiture,
+                "largeur_voiture" : largeur_voiture,
+                "hauteur_voiture" : hauteur_voiture,
+                "poids_vehicule" : poids_vehicule,
+                "type_moteur" : type_moteur,
+                "nombre_cylindres" : nombre_cylindres,
+                "taille_moteur" : taille_moteur,
+                "systeme_carburant" : systeme_carburant,
+                "taux_alésage" : taux_alésage,
+                "course" : course,
+                "taux_compression" : taux_compression,
+                "chevaux" : chevaux,
+                "tour_moteur" : tour_moteur,
+                "consommation_ville" : consommation_ville,
+                "consommation_autoroute" : consommation_autoroute,
+                "marque" : marque,
+                "modele" : modele
+                }, index=[0])
+    prediction = model.predict(user_inputs)
+    st.markdown("<h3 style='text-align: center;'>Le prix prédit de cette voiture est: {:.0f} $</h3>".format(prediction[0]), unsafe_allow_html=True)
+
 
 # Afficher le résultat de la prédiction
-st.write('Le prix prédit de cette voiture est:', prediction[0])
+
 
 
